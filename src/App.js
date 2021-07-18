710f95f7f98fb010a5c19c5d60e005c06b22e9cd
jmatondo <j_matondo@hotmail.com>
2021-07-18 19:12:06 +0200

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Layouts from "./components/Layouts";
+import Homes from "./pages/Homes";
+import Notes from "./pages/Notes";
 
 function App() {
-  return <div className="App">Ceci est le site d'EBM</div>;
+  return (
+    <>
+      <Router>
+        <Layouts>
+          <Switch>
+            <Route exact path="/">
+              <Homes />
+            </Route>
+            <Route path="/notes">
+              <Notes />
+            </Route>
+          </Switch>
+        </Layouts>
+      </Router>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Layouts.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Homes from "../pages/Homes";
+import Notes from "../pages/Notes";
+
+const Layouts = () => {
+  return (
+    <div>
+      Layouts
+      <Homes />
+      <Notes />
+    </div>
+  );
+};
+
+export default Layouts;
